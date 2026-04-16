d099adabc4f35a02f058617b670cfe500288fc83
Kevin <reitererkvn@gmail.com>
2026-04-17 01:23:57 +0200

    Auto-Sync: 2026-04-17 01:23

--- .config/uwsm/env.d/20-theme.sh
+++ .config/uwsm/env.d/20-theme.sh
@@ -14,7 +14,7 @@ export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 
 # Wallpapers
-export WALLPAPER0="$HOME/Bilder/wallpapers/Archive_desktop/Forest 1 uw.png" # day
+export WALLPAPER0="$HOME/Bilder/wallpapers/Forest1-uw.png" # day
 export WALLPAPER1="$HOME/Bilder/wallpapers/Archive_desktop/Forest 3 uw.png" # sunrise
 export WALLPAPER2="$HOME/Bilder/wallpapers/Archive_desktop/Forest 2 uw.png" # sunset
 export WALLPAPER3="$HOME/Bilder/wallpapers/Wallpaper 4 5120x1440.png" # night
