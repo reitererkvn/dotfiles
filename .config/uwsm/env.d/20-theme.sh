833914536b18c495175379ccd53175bf9ef5f417
Kevin <reitererkvn@gmail.com>
2026-06-12 10:46:43 +0200

    style: update system font to FiraMono Nerd Font Propo
    Switch the default system

--- .config/uwsm/env.d/20-theme.sh
+++ .config/uwsm/env.d/20-theme.sh
@@ -6,7 +6,7 @@ export HYPRCURSOR_SIZE=24
 export XCURSOR_THEME="Bibata-Modern-Classic"
 export HYPRCURSOR_THEME="Bibata-Modern-Classic"
 export GTK_THEME="Adwaita:dark"
-export FONT="FiraCode Nerd Font"
+export FONT="FiraMono Nerd Font Propo"
 export GTK_ICON_THEME_NAME="Papirus-Dark"
 
 # Qt-Architektur (Deaktiviert CSD-Schatten für sauberes Hyprland-Rendering)
