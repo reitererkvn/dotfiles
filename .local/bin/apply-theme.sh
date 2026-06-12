844bdea35f772ff4f89a467b957863083a0f96ce
Kevin <reitererkvn@gmail.com>
2026-06-12 13:48:26 +0200

    chore(hypr): remove deprecated keybinds configuration file
    Delete the unused and deprecated `hyprkeybinds.conf` file from the
    deprecated directory to clean up the configuration repository.
    
    chore(hypr): remove deprecated configuration files
    
    Remove unused configuration files from the deprecated directory
    
    refactor(power-menu): replace GTK power menu with wlogout
    
    - Remove unused

--- .local/bin/apply-theme.sh
+++ .local/bin/apply-theme.sh
@@ -89,6 +89,9 @@ done < <(find "$HOME/.config/hypr/assets" -name "*.template*")
 # yazi cant source, so must recieve a direct copy to its directory
 envsubst < "$HOME/.config/hypr/assets/yazi-theme.xtemplate" > "$HOME/.config/yazi/theme.toml"
 
+# wlogout
+envsubst "$envsubst_list" < "$HOME/.config/wlogout/style.template.css" > "$HOME/.config/wlogout/style.css"
+
 # GTK-settings
 envsubst < "$HOME/.config/hypr/assets/gtk-settings.xtemplate.ini" > "$HOME/.config/gtk-3.0/settings.ini"
 envsubst < "$HOME/.config/hypr/assets/gtk-settings.xtemplate.ini" > "$HOME/.config/gtk-4.0/settings.ini"
