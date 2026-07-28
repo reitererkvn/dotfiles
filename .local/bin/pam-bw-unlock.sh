6fd7f4251910fc61be4f45351f325e0319e7d429
Kevin <reitererkvn@gmail.com>
2026-07-29 00:08:02 +0200

    fix: set HOME variable for bitwarden cli in pam script

--- .local/bin/pam-bw-unlock.sh
+++ .local/bin/pam-bw-unlock.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# /usr/local/bin/pam-bw-unlock.sh
-# Unlocks Bitwarden automatically during PAM login
+exec > /var/log/pam-bw-unlock.log 2>&1
+set -x
 
-# Only trigger for the main user
 if [ "$PAM_USER" != "kevin" ]; then
     exit 0
 fi
 
-# Read password from standard input (passed by PAM expose_authtok)
 read -r PASSWORD
 export BW_PASSWORD="$PASSWORD"
+export HOME=/root
 
 SESSION_DIR="/run/vault"
 SESSION_FILE="$SESSION_DIR/bw_session"
@@ -17,12 +16,12 @@ SESSION_FILE="$SESSION_DIR/bw_session"
 mkdir -p "$SESSION_DIR"
 chmod 755 "$SESSION_DIR"
 
-# Unlock root vault (since background services like gdrive-live-sync use root context via sudo)
 BW_SESSION_TMP=$(/usr/bin/bw unlock --passwordenv BW_PASSWORD --raw)
 
 if [ $? -eq 0 ] && [ -n "$BW_SESSION_TMP" ]; then
     printf "%s" "$BW_SESSION_TMP" > "$SESSION_FILE"
     chmod 600 "$SESSION_FILE"
+    chown root:root "$SESSION_FILE"
 fi
 
 exit 0
